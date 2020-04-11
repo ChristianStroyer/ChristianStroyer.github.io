463bb646b9e220b04a4c6958d04593605673886c
sebastianstroyer <sebastian.stroyer@gmail.com>
2020-04-11 17:22:08 +0200

    Sebastians commit

--- js/liste.js
+++ js/liste.js
@@ -1,23 +1,39 @@
 var data = {
 	"punkter": [
 		{
-			"navn": 		"bil spil",
-			"tekst": 		"dette er en tekst om bil spil",
-			"billede": 		"/assets/bil_spil.png",
-			"link": 		"bil_spil.html",
+			"navn": 		"SHOOTER GAME",
+			"tekst": 		"dette er en tekst om shooter game",
+			"billede": 		"/assets/shooter_game.png",
+			"link": 		"/spil/shooter game/html.html",
 			"type": 		"spil",
-			"lavet": 		"05-04-2020",
-			"opdateret": 	"10-04-2020"
+			"lavet": 		"11 marts 2020"
 		},
 		{
-			"navn": 		"bil spil",
+			"navn": 		"PING PONG",
+			"tekst": 		"dette er en tekst om pingpong spillet",
+			"billede": 		"/assets/pingpong.png",
+			"link": 		"/spil/pingpong/pingpong.html",
+			"type": 		"spil",
+			"lavet": 		"19 febuar 2020"
+		},
+		{
+			"navn": 		"EPICK LABYRINT",
+			"tekst": 		"dette er en tekst om epick labyrint spillet",
+			"billede": 		"/assets/epick_labyrint.png",
+			"link": 		"/spil/epick labyrint/p5 skabelon/epick labyrint.html",
+			"type": 		"spil",
+			"lavet": 		"startede 15 januar 2020. blev færdig senere"
+		},
+		{
+			"navn": 		"BIL SPIL",
 			"tekst": 		"dette er en tekst om bil spil",
 			"billede": 		"/assets/bil_spil.png",
-			"link": 		"bil_spil.html",
+			"link": 		"/spil/bil spil/bil spil.html",
 			"type": 		"spil",
-			"lavet": 		"05-04-2020"/*,
-			"opdateret": 	"10-04-2020"*/
-		},
+			"lavet": 		"05-04-2020",
+			"opdateret": 	"1 december 2019"
+		}
+		
 	]
 }
 

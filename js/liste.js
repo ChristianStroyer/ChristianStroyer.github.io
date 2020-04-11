ad69aebec019d4b8618fb41e5b2a99622e7c7741
sebastianstroyer <sebastian.stroyer@gmail.com>
2020-04-11 17:25:07 +0200

    Sebastians commit

--- js/liste.js
+++ js/liste.js
@@ -5,33 +5,36 @@ var data = {
 			"tekst": 		"dette er en tekst om shooter game",
 			"billede": 		"/assets/shooter_game.png",
 			"link": 		"/spil/shooter game/html.html",
-			"type": 		"spil",
+			"type": 		"spil forsøg",
 			"lavet": 		"11 marts 2020"
+			"opdateret": 	""
 		},
 		{
 			"navn": 		"PING PONG",
 			"tekst": 		"dette er en tekst om pingpong spillet",
 			"billede": 		"/assets/pingpong.png",
 			"link": 		"/spil/pingpong/pingpong.html",
-			"type": 		"spil",
+			"type": 		"spil forsøg",
 			"lavet": 		"19 febuar 2020"
+			"opdateret": 	""
 		},
 		{
 			"navn": 		"EPICK LABYRINT",
 			"tekst": 		"dette er en tekst om epick labyrint spillet",
 			"billede": 		"/assets/epick_labyrint.png",
 			"link": 		"/spil/epick labyrint/p5 skabelon/epick labyrint.html",
-			"type": 		"spil",
+			"type": 		"spil forsøg",
 			"lavet": 		"startede 15 januar 2020. blev færdig senere"
+			"opdateret": 	""
 		},
 		{
 			"navn": 		"BIL SPIL",
 			"tekst": 		"dette er en tekst om bil spil",
 			"billede": 		"/assets/bil_spil.png",
 			"link": 		"/spil/bil spil/bil spil.html",
-			"type": 		"spil",
-			"lavet": 		"05-04-2020",
-			"opdateret": 	"1 december 2019"
+			"type": 		"spil forsøg",
+			"lavet": 		"1 december 2019",
+			"opdateret": 	"19 marts 2020"
 		}
 		
 	]

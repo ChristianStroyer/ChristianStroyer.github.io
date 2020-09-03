f5253ec837f139094e14758ff293887b481bf1d5
sebastianstroyer <sebastian.stroyer@gmail.com>
2020-09-03 20:07:49 +0200

    Sebastians commit

--- bog_uddrrag/bog_viser/js.js
+++ bog_uddrrag/bog_viser/js.js
@@ -1,13 +1,18 @@
 console.log ("Working")
 
-console.log("Working")
+
+var books = {
+	//	bog_navn serie_navn forfatter	[dato,sidetal , dato,sidetal , dato,sidetal]
+}
+
+
 
 function setup () {
 	createCanvas(750,500)
 }
 
 function draw () {
-	
+
 	
 	opdate_wiew()
 }

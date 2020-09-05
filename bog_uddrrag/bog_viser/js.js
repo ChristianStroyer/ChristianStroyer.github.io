1a9788c7a3c1cae078e55951bc55056d48b00bca
sebastianstroyer <sebastian.stroyer@gmail.com>
2020-09-05 13:08:15 +0200

    Sebastians commit

--- bog_uddrrag/bog_viser/js.js
+++ bog_uddrrag/bog_viser/js.js
@@ -5,10 +5,10 @@ var books = {
 
 "books":
 	[
-		{"title":"Dæmonernes by","serie":"dødens instrumenter","sider":[0,43,109,142,197,231,246,327,363,393,421,448]},
-		{"title":"Den Tavse by","serie":"dødens instrumenter","sider":[0,41,69,102,136,156,184,308,387,403]},
-		{"title":"Englenes by","serie":"dødens instrumenter","sider":[0,142,167,204,288,325,392,438,457,475,490]},
-		{"title":"de faldene engles by","serie":"dødens instrumenter","sider":[0,59,65,85,104,116,141,146,154,164,183,193,209,232,247,258,273,294,308,319,329,337,345,354,362,369,377]},
+		{"title":"Dæmonernes by",			"serie":"dødens instrumenter",	"sider":[0,43,109,142,197,231,246,327,363,393,421,448]},
+		{"title":"Den Tavse by",			"serie":"dødens instrumenter",	"sider":[0,41,69,102,136,156,184,308,387,403]},
+		{"title":"Englenes by",				"serie":"dødens instrumenter",	"sider":[0,142,167,204,288,325,392,438,457,475,490]},
+		{"title":"de faldene engles by",	"serie":"dødens instrumenter",	"sider":[{"side":0,"dato":"2020-08-03"},{"side":59,"dato":"2020-08-04"},{"side":65,"dato":"2020-08-05"},{"side":85,"dato":"2020-08-06"},{"side":104,"dato":"2020-08-07"},{"side":116,"dato":"2020-08-08"},{"side":141,"dato":"2020-08-09"},{"side":146,"dato":"2020-08-10"},{"side":154,"dato":"2020-08-11"},{"side":164,"dato":"2020-08-12"},{"side":183,"dato":"2020-08-13"},{"side":193,"dato":"2020-08-14"},{"side":209,"dato":"2020-08-15"},{"side":232,"dato":"2020-08-16"},{"side":247,"dato":"2020-08-17"},{"side":258,"dato":"2020-08-18"},{"side":273,"dato":"2020-08-19"},{"side":294,"dato":"2020-08-20"},{"side":308,"dato":"2020-08-21"},{"side":319,"dato":"2020-08-22"},{"side":329,"dato":"2020-08-23"},{"side":337,"dato":"2020-08-24"},{"side":345,"dato":"2020-08-25"},{"side":354,"dato":"2020-08-26"},{"side":362,"dato":"2020-08-27"},{"side":369,"dato":"2020-08-28"},{"side":377,"dato":"2020-08-29"}]},
 	]
 }
 

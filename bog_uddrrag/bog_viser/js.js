70db437c56c950e1f5925080a1d7b8dd6a07ff2b
ChristianStroyer <ChristianStroyer@users.noreply.github.com>
2020-09-05 12:19:49 +0200

    Json

--- bog_uddrrag/bog_viser/js.js
+++ bog_uddrrag/bog_viser/js.js
@@ -2,10 +2,15 @@ console.log ("Working")
 
 
 var books = {
-	//	bog_navn serie_navn forfatter	[dato,sidetal , dato,sidetal , dato,sidetal
-	
-}
 
+"books":
+	[
+		{"title":"Dæmonernes by","serie":"dødens instrumenter","sider":[0,43,109,142,197,231,246,327,363,393,421,448]},
+		{"title":"Den Tavse by","serie":"dødens instrumenter","sider":[0,41,69,102,136,156,184,308,387,403]},
+		{"title":"Englenes by","serie":"dødens instrumenter","sider":[0,142,167,204,288,325,392,438,457,475,490]},
+		{"title":"de faldene engles by","serie":"dødens instrumenter","sider":[0,59,65,85,104,116,141,146,154,164,183,193,209,232,247,258,273,294,308,319,329,337,345,354,362,369,377]},
+	]
+}
 
 
 function setup () {

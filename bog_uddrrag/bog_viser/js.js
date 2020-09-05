08aaea91c229b42df215e3b0465f0b2d5897a980
sebastianstroyer <sebastian.stroyer@gmail.com>
2020-09-05 16:44:25 +0200

    Sebastians commit

--- bog_uddrrag/bog_viser/js.js
+++ bog_uddrrag/bog_viser/js.js
@@ -3,29 +3,40 @@ console.log ("Working")
 
 var books = {
 
-"books":
-	[
-		{"title":"Dæmonernes by",			"serie":"dødens instrumenter",	"sider":[0,43,109,142,197,231,246,327,363,393,421,448]},
-		{"title":"Den Tavse by",			"serie":"dødens instrumenter",	"sider":[0,41,69,102,136,156,184,308,387,403]},
-		{"title":"Englenes by",				"serie":"dødens instrumenter",	"sider":[0,142,167,204,288,325,392,438,457,475,490]},
-		{"title":"de faldene engles by",	"serie":"dødens instrumenter",	"sider":[{"side":0,"dato":"2020-08-03"},{"side":59,"dato":"2020-08-04"},{"side":65,"dato":"2020-08-05"},{"side":85,"dato":"2020-08-06"},{"side":104,"dato":"2020-08-07"},{"side":116,"dato":"2020-08-08"},{"side":141,"dato":"2020-08-09"},{"side":146,"dato":"2020-08-10"},{"side":154,"dato":"2020-08-11"},{"side":164,"dato":"2020-08-12"},{"side":183,"dato":"2020-08-13"},{"side":193,"dato":"2020-08-14"},{"side":209,"dato":"2020-08-15"},{"side":232,"dato":"2020-08-16"},{"side":247,"dato":"2020-08-17"},{"side":258,"dato":"2020-08-18"},{"side":273,"dato":"2020-08-19"},{"side":294,"dato":"2020-08-20"},{"side":308,"dato":"2020-08-21"},{"side":319,"dato":"2020-08-22"},{"side":329,"dato":"2020-08-23"},{"side":337,"dato":"2020-08-24"},{"side":345,"dato":"2020-08-25"},{"side":354,"dato":"2020-08-26"},{"side":362,"dato":"2020-08-27"},{"side":369,"dato":"2020-08-28"},{"side":377,"dato":"2020-08-29"}]},
-	]
+"books":[
+
+	{"title":"Det forsvundene tegn",				"serie":"langdon-serien (3)",		"sider":[0,30,55,62,79,116,138,167,190,203,221,274,285,319,342,389,414,446,479,494,522,552,577,586,616,638,661,676,680]},
+	{"title":"Inferno",								"serie":"langdon-serien (4)",		"sider":[0,26,48,64,87,117,140,164,200,224,258,276,303,326,350,375,408,432,480,500,527,547]},
+	{"title":"oprindelse",							"serie":"langdon-serien (5)",		"sider":[30,52,73,96,108,128,164,178,188,190,228,260,276,308,332,359,400,428,454,482,501,524,555]},
+	{"title":"The hitchhikers guid to the galaxy",	"serie":"a trilogy in four parts",	"sider":[0,30,50,62,80,92,102,110,116,128,142,150,172,186,198,204,212,214,216,219,224,229,237,251,257,261,273,278,289,300,306,316,322,328,336,343,346,360,368,278,383,392,400,408,414,420,428,440,446,449,460,467,472,475,480,484,490,496,504,508,518,522,524,526,528,560,574,579,584,590]},
+	{"title":"Tyranens Gravkamer",					"serie":"Apollons prøvelser",		"sider":[0,49,55,73,96,119,130,154,160,180,194,202,226,246,273,288,290,291,302,315,326,344,363,370,390,392,404,417,440,458,460,465]},
+	{"title":"9 fra de ni verdner",					"serie":"magnus chase",				"sider":[0,27,53,79,96,108,126,137]},
+	{"title":"Dæmonernes by",						"serie":"dødens instrumenter",		"sider":[0,43,109,142,197,231,246,327,363,393,421,448]},
+	{"title":"Den Tavse by",						"serie":"dødens instrumenter",		"sider":[0,41,69,102,136,156,184,308,387,403]},
+	{"title":"Englenes by",							"serie":"dødens instrumenter",		"sider":[0,142,167,204,288,325,392,438,457,475,490]},
+	{"title":"de faldene engles by",				"serie":"dødens instrumenter",		"sider":[0,59,65,85,104,116,141,146,154,164,183,193,209,232,247,258,273,294,308,319,329,337,345,354,362,369,377]}
+
+]
+
+
 }
 
 
 function setup () {
 	createCanvas(750,500)
+	opdate_wiew()
 }
 
 function draw () {
 
 	
-	opdate_wiew()
+	
 }
 
 
 function opdate_wiew () {
 	background(120)
+
 }
 
 
